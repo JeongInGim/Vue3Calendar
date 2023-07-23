c6c2604ef1bcf8368f613c82111b6047a48a1fc8
Kewon <yang0184@naver.com>
2023-07-23 14:17:16 +0900

    composition

--- src/components/body.vue
+++ src/components/body.vue
@@ -1,5 +1,5 @@
 <template>
-  <table>
+<!--  <table>
     <thead>
       <tr>
         <th v-for="day in DAYS">{{ day }}</th>
@@ -10,23 +10,23 @@
         <td v-for="date in week">{{ date }}</td>
       </tr>
     </tbody>
-  </table>
+  </table>-->
 
-<!--  <div class="calendar-area">
+  <div class="calendar">
     <div class="calendar-header">
-      <div class="calendar-title" v-for="day in DAYS">
+      <div class="calendar-header-context" v-for="day in DAYS">
         {{ day }}
       </div>
     </div>
 
     <div class="calendar-body">
-      <div v-for="week in dates">
-        <div v-for="date in week">
-          <div class="calendar-day">{{ date }}</div>
+      <div class="calendar-body-week" v-for="week in dates">
+        <div class="calendar-body-week-context" v-for="date in week">
+          {{ date }}
         </div>
       </div>
     </div>
-  </div>-->
+  </div>
 </template>
 
 <script setup>
@@ -36,42 +36,6 @@ const DAYS = [ '일', '월', '화', '수', '목', '금', '토' ];
 const dates = ref([]);
 
 onMounted(setDatesOfThisMonth)
-
-const getToday = () => {
-  const today = new Date()
-
-  return {
-    year: today.getFullYear(),
-    month: today.getMonth(),
-  }
-}
-
-const getFirst = () => {
-  const today = getToday();
-  const first = new Date(today.year, today.month, '01');
-
-  return {
-    day: first.getDay(),
-  }
-}
-/**
- * 마지막날 가져오기
- */
-const getLast = () => new Date(getToday().year, getToday().month + 1, 0);
-
-function getFirstWeek() {
-  const firstWeek = [];
-
-  const length = DAYS.length
-  for (let j = 0; j < getFirst().day; j++) {
-    firstWeek.push('');
-  }
-  for (let i = 0; i < length - getFirst().day; i++) {
-    firstWeek.push(i + 1);
-  }
-
-  return firstWeek;
-}
 
 function setDatesOfThisMonth() {
   const firstWeek = getFirstWeek();
@@ -97,6 +61,21 @@ function setDatesOfThisMonth() {
   }
 }
 
+function getFirstWeek() {
+  const firstWeek = [];
+
+  const length = DAYS.length
+  for (let j = 0; j < getFirst().day; j++) {
+    firstWeek.push('');
+  }
+  for (let i = 0; i < length - getFirst().day; i++) {
+    firstWeek.push(i + 1);
+  }
+
+  return firstWeek;
+}
+
+
 function setLastWeek() {
   // 마지막 날이 6요일이면
   // 스킵
@@ -110,24 +89,97 @@ function setLastWeek() {
   }
 }
 
+const getToday = () => {
+  const today = new Date()
+
+  return {
+    year: today.getFullYear(),
+    month: today.getMonth(),
+  }
+}
+
+const getFirst = () => {
+  const today = getToday();
+  const first = new Date(today.year, today.month, '01');
+
+  return {
+    day: first.getDay(),
+  }
+}
+/**
+ * 마지막날 가져오기
+ */
+const getLast = () => new Date(getToday().year, getToday().month + 1, 0);
 
 </script>
 
-<style>
-table {
-    display: inline;
-    font-size: 30px;
-}
-th, td {
-    padding: 10px 30px;
+<style lang="scss">
+//table {
+//    display: inline;
+//    font-size: 30px;
+//}
+//th, td {
+//    padding: 10px 30px;
+//    text-align: center;
+//    border: 1px solid black;
+//    font-size: 20px;
+//}
+//th:nth-child(1), td:nth-child(1) {
+//    color: red;
+//}
+//th:nth-child(7), td:nth-child(7) {
+//    color: blue;
+//}
+
+
+/********************************/
+
+.calendar {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 2px;
+
+  &-header-context, &-body-week-context {
+    width: 84px;
+    padding: 10px 0;
     text-align: center;
     border: 1px solid black;
     font-size: 20px;
+  }
+
+  &-header {
+    display: flex;
+    column-gap: 2px;
+
+    &-context:nth-of-type(1) {
+      color: red;
+    }
+
+    &-context:nth-of-type(7) {
+      color: blue;
+    }
+  }
+
+  &-body {
+    display: flex;
+    flex-direction: column;
+    row-gap: 2px;
+
+    &-week {
+      display: flex;
+      column-gap: 2px;
+
+      &-context:nth-of-type(1) {
+        color: red;
+      }
+
+      &-context:nth-of-type(7) {
+        color: blue;
+      }
+    }
+  }
 }
-th:nth-child(1), td:nth-child(1) {
-    color: red;
-}
-th:nth-child(7), td:nth-child(7) {
-    color: blue;
-}
+
+
 </style>
